3d286bdef5dd17764dcddffabb93bbb36081bb52
Martin Gogulanov <152088770+MonsterGuti@users.noreply.github.com>
2025-06-26 16:57:45 +0300

    Add Trainer and Pokemon classes with catching and releasing logic

--- Advanced_OOP/first_steps_in_OOP - Exercise/pokemon_battle/project/trainer.py
+++ Advanced_OOP/first_steps_in_OOP - Exercise/pokemon_battle/project/trainer.py
@@ -1,4 +1,4 @@
-from pokemon import Pokemon
+from project.pokemon import Pokemon
 
 class Trainer:
     def __init__(self, name: str):
@@ -12,18 +12,15 @@ class Trainer:
         return "This pokemon is already caught"
 
     def release_pokemon(self, pokemon_name: str):
-        for p in self.pokemons:
-            if p.name == pokemon_name:
-                self.pokemons.remove(p)
+        for pokemon in self.pokemons:
+            if pokemon.name == pokemon_name:
+                self.pokemons.remove(pokemon)
                 return f"You have released {pokemon_name}"
         return "Pokemon is not caught"
 
     def trainer_data(self):
-        result = ""
-        result += f"Pokemon Trainer {self.name}\nPokemon count {len(self.pokemons)}\n"
+        result = f"Pokemon Trainer {self.name}\nPokemon count {len(self.pokemons)}"
         for pokemon in self.pokemons:
-            result += f"- {pokemon.pokemon_details()}\n"
+            result += f"\n- {pokemon.pokemon_details()}"
         return result
-
-
 

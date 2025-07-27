9f96435d4eda3348c17849d31882e8b17cf7f36e
MonsterGuti <gogulanovmartin@gmail.com>
2025-07-27 06:52:41 +0300

    finding prime numbers in a list using generator

---
+++ Advanced_OOP/Exercise - Iterators and Generators/prime_numbers.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+
+def get_primes(numbers):
+    for num in numbers:
+        if num < 2:
+            continue
+        for i in range(2, int(sqrt(num)) + 1):
+            if num % i == 0:
+                break
+        else:
+            yield num
